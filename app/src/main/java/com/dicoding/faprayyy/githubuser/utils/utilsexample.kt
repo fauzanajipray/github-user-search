1d2b3902f4315c610d01259c28c985a2db3732d7
faprayyy <fauzanjr1@gmail.com>
2022-06-26 11:05:58 +0700

    add utilsexample.kt

---
+++ app/src/main/java/com/dicoding/faprayyy/githubuser/utils/utilsexample.kt
@@ -0,0 +1,8 @@
+package com.dicoding.faprayyy.githubuser.utils
+
+//Copy file ini, dan ganti nama file menjadi utils.kt, Generate APIKEY dari GITHUB dan isikan dibawah
+
+//object utils {
+//
+//    val apiKey = "PersonalAksesTokenGithub"
+//}
